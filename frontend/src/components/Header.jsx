64abbe485ee33dd0c2810e60444e6d8509062348
Tin <tinmikic7@gmail.com>
2025-12-17 18:06:18 +0100

    Gotov dropdown menu

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import '../index.css'
 
 const Header = () => {
-    /*const [LoggedIn, setLoggedIn] = useState(false);*/ 
+    /*LoggedIn varijabla je temp var za togglanje vrste dropdowna kasnije treba promijeniti kad napravimo login logic*/ 
+    const LoggedIn = true; 
     const [visible, setVisible] = useState(false);
-    
+    const dropdownRef = useRef(null);
+
     const buttonHandler = () => {
         setVisible(current => !current); 
         {console.log("Dropdown menu"+visible)};    
-    }
+    };
+
+    useEffect(() => {
+    const handleClickOutside = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setVisible(false);
+        }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+    };
+    }, []);
 
     return (
         <header>
@@ -24,7 +40,7 @@ const Header = () => {
                     </Link>
                     <button className="btn btn-primary">Objavi prijevoz</button>
 
-                    <div className="relative inline-block avatar-container">
+                    <div className="relative inline-block avatar-container" ref={dropdownRef}>
                     <button className="avatar" onClick={buttonHandler}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
@@ -32,11 +48,20 @@ const Header = () => {
                     </button>
 
                     {visible && (
-                        <div className="dropdown-menu">
-                            <div className="">
-                            Dropdown is open
-                            </div>
-                        </div>
+                        <>
+                            {LoggedIn ? (
+                                <div className="dropdown-menu">
+                                <Link ><div className='text-white dropdown-item'>Profil</div></Link>
+                                <Link ><div className='text-white dropdown-item'>Postavke</div></Link>
+                                <div class="dropdown-divider"></div>
+                                <Link ><div className='text-white dropdown-item'>Odjava</div></Link>
+                                </div>
+                            ) : (
+                                <div className='dropdown-menu-transparent'>
+                                <button className='btn btn-primary'><Link to="/login"><div className='text-white'>Prijavi se</div></Link></button>
+                                </div>
+                            )}
+                        </>
                     )}
                     </div>
                 </div>
