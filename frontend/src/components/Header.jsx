45f4f3a5a595b9a1326c8b82364addbafcef1aad
Tin <tinmikic7@gmail.com>
2025-12-17 15:49:45 +0100

    Dodao da kad se klikne avatar mijenja se state

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from "react";
+
 
 const Header = () => {
+    /*const [LoggedIn, setLoggedIn] = useState(false);*/ 
+    const [visible, setVisible] = useState(false);
+    
+    const buttonHandler = () => {
+        setVisible(current => !current); 
+        {console.log("Dropdown menu"+visible)};    
+    }
+
     return (
         <header>
             <nav>
@@ -13,11 +23,11 @@ const Header = () => {
                         <button className="btn btn-ghost">Prijava</button>
                     </Link>
                     <button className="btn btn-primary">Objavi prijevoz</button>
-                    <div className="avatar">
+                    <button className="avatar" onClick={buttonHandler}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                         </svg>
-                    </div>
+                    </button>
                 </div>
             </nav>
         </header>
